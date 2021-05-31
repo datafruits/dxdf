a48d9d4aa340b3c09ccb49d9b69caa6f9d43db28
Tony Miller <mcfiredrill@gmail.com>
2021-05-31 12:55:31 +0900

    update dates

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,8 +7,8 @@ export default class ApplicationRoute extends Route {
     let query = {};
     query.timezone = jstz.determine().name();
 
-    let start = "2020-03-27";
-    let end = "2020-04-01";
+    let start = "2021-05-31";
+    let end = "2021-06-08";
     query.start = moment(start).startOf('day').format('YYYY-MM-DD');
     query.end = moment(end).endOf('day').format('YYYY-MM-DD');
     return this.store.query('scheduled-show', query);
